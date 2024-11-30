e1549ec71b05c66b33c19db37051e8cf2faf70d4
volka <volka5091@gmail.com>
2024-12-01 01:54:51 +0900

    [service] 라우팅 테스트

--- second-service/src/main/java/com/volka/ecommerce/secondservice/SecondController.java
+++ second-service/src/main/java/com/volka/ecommerce/secondservice/SecondController.java
@@ -1,11 +1,20 @@
 package com.volka.ecommerce.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RequestMapping("/api/v1/second")
+@RequestMapping("/second-service")
 @RestController
 public class SecondController {
+
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        System.out.println(header);
+        return "Hello World in Second Service";
+    }
 }
