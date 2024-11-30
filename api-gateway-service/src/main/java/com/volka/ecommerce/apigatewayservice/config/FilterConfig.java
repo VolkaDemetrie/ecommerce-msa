4151b0fdd5701ae5c6ddcb1bc4ab953307530e40
volka <volka5091@gmail.com>
2024-12-01 01:25:37 +0900

    [INIT] 멀티모듈

---
+++ api-gateway-service/src/main/java/com/volka/ecommerce/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,11 @@
+package com.volka.ecommerce.apigatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+//    @Bean
+//    public Route
+}
---
+++ second-service/src/main/java/com/volka/ecommerce/secondservice/SecondController.java
@@ -0,0 +1,11 @@
+package com.volka.ecommerce.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/v1/second")
+@RestController
+public class SecondController {
+}
