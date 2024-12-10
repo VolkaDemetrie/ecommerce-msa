53040bc6f99d5476647c557e84018ebdb23ad7f6
volka <volka5091@gmail.com>
2024-12-10 23:54:03 +0900

    유저 서비스 컨트롤러, 설정 추가

---
+++ user-service/src/main/java/com/volka/ecommerce/userservice/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.volka.ecommerce.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/user-service")
+@RestController
+public class UserController {
+
+    private final Environment env;
+
+    @GetMapping("/health-check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
+    }
+}
