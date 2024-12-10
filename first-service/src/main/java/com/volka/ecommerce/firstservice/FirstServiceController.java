6ebc0e2f224049da26427b00f62e1e1ba8d94562
volka <volka5091@gmail.com>
2024-12-10 22:57:26 +0900

    게이트웨이 유레카 연동

--- first-service/src/main/java/com/volka/ecommerce/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/volka/ecommerce/firstservice/FirstServiceController.java
@@ -1,13 +1,21 @@
 package com.volka.ecommerce.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/first-service")
 @RestController
 public class FirstServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -21,7 +29,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. First service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. First service on PORT %s", env.getProperty("local.server.port"));
     }
 }
--- second-service/src/main/java/com/volka/ecommerce/secondservice/SecondController.java
+++ second-service/src/main/java/com/volka/ecommerce/secondservice/SecondController.java
@@ -1,16 +1,21 @@
 package com.volka.ecommerce.secondservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/second-service")
 @RestController
 public class SecondController {
 
+    private final Environment env;
 
     @GetMapping("/message")
     public String message(@RequestHeader("second-request") String header) {
@@ -19,7 +24,8 @@ public class SecondController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. Second service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. Second service on PORT %s", env.getProperty("local.server.port"));
     }
 }
